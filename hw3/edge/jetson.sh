233a56e26d9522f6c3017a20819349b2d59d7421
Rachael Burns <rkburns@berkeley.edu>
2019-05-30 07:03:28 -0400

    experimenting with bridge instead of forwarder

--- hw3/edge/jetson.sh
+++ hw3/edge/jetson.sh
@@ -4,12 +4,12 @@ docker network create --driver bridge jetsonbridge
 
 docker build -t broker -f Dockerfile.broker .
 
-docker build -t forwarder -f Dockerfile.forwarder .
+#docker build -t forwarder -f Dockerfile.forwarder .
 
 docker build -t facedetect -f Dockerfile.facedetect .
 
 docker run -d --name broker --network jetsonbridge broker
 
-docker run -d --name forwarder --network jetsonbridge forwarder
+#docker run -d --name forwarder --network jetsonbridge forwarder
 
 docker run -d --name facedetect --privileged --network jetsonbridge facedetect
