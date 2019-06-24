1063dc29560356f1a51637b6d6dafc0cc5a4bc28
Rachael Burns <rkburns@berkeley.edu>
2019-06-23 17:37:06 -0700

    update

--- posture_ingestion/cloud/cloud.sh
+++ posture_ingestion/cloud/cloud.sh
@@ -4,8 +4,9 @@ docker network create --driver bridge cloudbridge
 
 docker build -t cloudbroker -f Dockerfile.cloudbroker .
 
-docker build -t readandsave -f Dockerfile.readandsave .
-
+docker build -t readandsave_slouch -f Dockerfile.readandsave_slouch .
+docker build -t readandsave_straight -f Dockerfile.readandsave_straight .
 docker run -d --name cloudbroker -p 1883:1883 --network cloudbridge cloudbroker
 
-docker run -d --name readandsave -v /mnt/hw3bucket:/data --network cloudbridge readandsave
+docker run -d --name readandsave_slouch -v /mnt/hw3bucket:/data --network cloudbridge readandsave
+docker run -d --name readandsave_straight -v /mnt/hw3bucket:/data --network cloudbridge readandsave
