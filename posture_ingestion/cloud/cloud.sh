c82655480b51ad721e6cdefbd70ee789b51fbc60
root <root@hw07.w251.mids>
2019-06-25 03:20:02 +0000

    update

--- posture_ingestion/cloud/cloud.sh
+++ posture_ingestion/cloud/cloud.sh
@@ -8,5 +8,5 @@ docker build -t readandsave_slouch -f Dockerfile.readandsave_slouch .
 docker build -t readandsave_straight -f Dockerfile.readandsave_straight .
 docker run -d --name cloudbroker -p 1883:1883 --network cloudbridge cloudbroker
 
-docker run -d --name readandsave_slouch -v /mnt/hw3bucket:/data --network cloudbridge readandsave
-docker run -d --name readandsave_straight -v /mnt/hw3bucket:/data --network cloudbridge readandsave
+docker run -d --name readandsave_slouch -v /mnt/hw3bucket:/data --network cloudbridge readandsave_slouch
+docker run -d --name readandsave_straight -v /mnt/hw3bucket:/data --network cloudbridge readandsave_straight
